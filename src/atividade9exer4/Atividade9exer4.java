2acd338d8b222330fa9db89e005371c9f4e9f155
Lucas Friedrich <ntc.lucassilva@gmail.com>
2021-06-06 11:50:45 -0300

    Projeto criado dia 06/06/21 - Exercicio lógica, subindo para Github...

---
+++ src/atividade9exer4/Atividade9exer4.java
@@ -0,0 +1,15 @@
+package atividade9exer4;
+/**
+ *
+ * @author Lucas
+ */
+public class Atividade9exer4 {
+    public static void main(String[] args) {
+        int numero;
+        numero = 101;
+        do{
+            System.out.println(numero);
+            numero = numero + 2;
+        }while(200 > numero);
+    }   
+}
